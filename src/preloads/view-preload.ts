3941910541652dba60162a2bae00575ba41bf869
Damon <137374946+s3nix-sys@users.noreply.github.com>
2025-09-05 20:27:32 -0700

    moved shim.ts to the view preload

--- src/preloads/view-preload.ts
+++ src/preloads/view-preload.ts
@@ -1,4 +1,48 @@
-import { contextBridge, ipcRenderer, webFrame } from 'electron';
+import { contextBridge, ipcRenderer as electronIpcRenderer, webFrame } from 'electron';
+(window as any).ipcRenderer = electronIpcRenderer;
+
+
+export type IpcRendererLike = {
+  send: (...args: any[]) => void;
+  sendSync?: (...args: any[]) => any;
+  invoke?: (...args: any[]) => Promise<any>;
+  on: (channel: string, listener: (...args: any[]) => void) => any;
+  once: (channel: string, listener: (...args: any[]) => void) => any;
+  removeListener: (channel: string, listener: (...args: any[]) => void) => any;
+  removeAllListeners: (channel?: string) => any;
+  postMessage?: (channel: string, message: any, transfer?: any[]) => void;
+};
+
+declare global {
+  interface Window {
+    __electronApi?: { ipcRenderer: IpcRendererLike; remote?: any };
+    electron?: { ipcRenderer?: IpcRendererLike; remote?: any };
+    ipcRenderer?: IpcRendererLike;
+    remote?: any;
+  }
+}
+
+function pickIpc(): IpcRendererLike {
+  const api = window.__electronApi?.ipcRenderer
+    ?? window.electron?.ipcRenderer
+    ?? (window as any).ipcRenderer;
+  if (!api) {
+
+    const no = () => {};
+    const noop: any = new Proxy(no, { get: () => noop, apply: () => undefined });
+    return Object.assign(noop, { on: no, once: no, removeListener: no, removeAllListeners: no });
+  }
+  return api;
+}
+
+function pickRemote(): any {
+  return window.__electronApi?.remote ?? window.electron?.remote ?? (window as any).remote ?? {};
+}
+
+export const ipcRenderer: IpcRendererLike = pickIpc();
+export const remote: any = pickRemote();
+
+
 
 import AutoComplete from './models/auto-complete';
 import { ERROR_PROTOCOL, WEBUI_BASE_URL } from '~/constants/files';
